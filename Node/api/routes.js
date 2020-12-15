eb1e7d8ae06ac0e7517a374984c97603f4e93d3b
ErwanDelory <erwan.delory@gmail.com>
2020-12-15 11:56:58 +0100

    update

--- Node/api/routes.js
+++ Node/api/routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const enregistrement = require('../controllers/enregistrement');
-
 const router = express.Router();
 
-//Test
 router.post('/enregistrement', enregistrement.recherches);
 
 module.exports = router;
